135cbdc2d7fc9fcfa8dd70cd481db3579956257f
Hibram007 <87105978+Hibram007@users.noreply.github.com>
2021-08-22 13:42:56 -0700

    variables defined

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,41 @@
 // Assignment code here
 
+// Define/layout what the input variables are: 
+var enter;
+var confirmNumber;
+var confirmCharacter;
+var confirmUppercase;
+var confirmLowercase;
 
-// Get references to the #generate element
+// Variable values= character types to select from: 
+
+// Special characters -- others are self explanatory
+character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+space = [];
+
+// Choices declared outside the if statement so they can be concatenated upon condition
+var choices;
+// converts letters to uppercase 
+var toUpper = function (x) {
+    return x.toUpperCase();
+};
+// creates a variable for uppercase conversion
+alpha2 = alpha.map(toUpper);
+
 var generateBtn = document.querySelector("#generate");
+
+generateBtn.addEventListener("click", function () {
+    gp = generatePassword();
+    document.getElementById("password").placeholder = gp;
+});
+
+
+
 
 // Write password to the #password input
 function writePassword() {
